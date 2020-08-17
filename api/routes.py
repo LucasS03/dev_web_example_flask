7f2a5858ed8ae2be7cf78c67bb5dee97b4d7b46e
Lucas Sena <slsena45@gmail.com>
2020-08-17 20:10:41 -0300

    first commit example flask

---
+++ api/routes.py
@@ -0,0 +1,6 @@
+from flask import render_template
+from api import app
+
+@app.route('/')
+def hello():
+  return render_template('index.html')
---
+++ run.py
@@ -0,0 +1,6 @@
+from api import app
+
+if __name__ == '__main__':
+  app.run()
+  # or
+  # app.run(debug = True)
